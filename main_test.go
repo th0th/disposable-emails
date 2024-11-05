1be7eb25e22dff2406d134803eb5d2a19f687628
Gokhan Sari <gokhan@sari.me>
2024-11-05 18:49:49 +0300

    Add first implementation

---
+++ main_test.go
@@ -0,0 +1,60 @@
+package disposable_emails
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_service_Check(t *testing.T) {
+	type fields struct {
+		domainsMap map[string]bool
+	}
+	type args struct {
+		emailOrDomain string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *CheckResult
+	}{
+		{
+			name: "disposable domain",
+			fields: fields{
+				domainsMap: map[string]bool{
+					"domain.tld": true,
+				},
+			},
+			args: args{
+				emailOrDomain: "user@domain.tld",
+			},
+			want: &CheckResult{
+				IsDisposable: true,
+			},
+		},
+		{
+			name: "non-disposable domain",
+			fields: fields{
+				domainsMap: map[string]bool{
+					"domain.tld": true,
+				},
+			},
+			args: args{
+				emailOrDomain: "user@domain2.tld",
+			},
+			want: &CheckResult{
+				IsDisposable: false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &service{
+				domainsMap: tt.fields.domainsMap,
+			}
+			if got := s.Check(tt.args.emailOrDomain); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Check() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
